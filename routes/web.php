cd2db298c337d51aa1c9b5ff55ab8966671392a8
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-10-05 12:11:10 +0545

    Ui bug fixes

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
