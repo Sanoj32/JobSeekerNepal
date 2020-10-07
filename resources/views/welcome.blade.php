7c487de3bef2fe4eeaa76deea44606ba9d4c4483
Sandzzzz <sunsetsin1.sy@gmail.com>
2020-10-07 17:51:33 +0545

    name changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Pravat</title>
     <script src="https://use.fontawesome.com/b0f225cc8a.js"></script>
 
     <!-- Fonts -->
