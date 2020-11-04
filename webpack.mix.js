d339f9326efa995d72dc74ab20c09817de1e4da9
Sandzzzz <sunsetsin1.sy@gmail.com>
2020-11-05 02:20:51 +0545

    footer added

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+    mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
