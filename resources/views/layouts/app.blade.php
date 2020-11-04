ab23e65c94d6cb4506676709bc93170e2f975bc3
Sandzzzz <sunsetsin1.sy@gmail.com>
2020-11-05 02:27:51 +0545

    space up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,7 +126,6 @@ use Illuminate\Support\Facades\DB;
             <ul class="social-icons">
               <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
               <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
-              
               <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
             </ul>
           </div>
