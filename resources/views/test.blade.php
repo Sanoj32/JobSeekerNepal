8daafaddb53de47d58bd3c4987bf0e29a0f9944b
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-11-08 11:52:42 +0545

    Linkedin and merorojgari dates fixed

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,13 +1,23 @@
 
 <?php
 use App\Jobs;
+use Carbon\Carbon;
 
-$jobs = Jobs::where('url', 'ILIKE', '%linkedin%')->get();
+$datenow = Carbon::now('Asia/Kathmandu'); //the exact date of today
+$datenow->format('Y-m-d');
+
+$jobs = Jobs::where('url', 'ILIKE', '%merorojgari%')->get();
 foreach ($jobs as $job) {
-    $job->isExpired    = false;
-    $deadline          = $job->deadline;
-    $deadline          = date('Y-m-d', strtotime($deadline));
+    $job->isExpired = false;
+    $deadline       = $job->deadline;
+    echo $deadline;
+    echo "<br>";
     $truedeadline      = date('Y-m-d', strtotime("+1 month", strtotime($deadline)));
     $job->truedeadline = $truedeadline;
+    if ($job->truedeadline < $datenow) {
+        $job->isExpired = true;
+    }
+    echo $job->truedeadline;
+    echo "<hr>";
     $job->save();
 }
