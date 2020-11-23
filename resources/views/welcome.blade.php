c2aacf4e53508b37be8aa4ce0e9b1ba8d069b19e
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-11-23 21:59:33 +0545

    Bar graph used

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -310,7 +310,7 @@ foreach ($websiteNames as $websiteName) {
 
                                         <div class="col-md-6">
                                             <div class="card">
-                                                <div class="card-header text-center">Websites</div>
+                                                <div class="card-header te">Websites</div>
                                                 <div class="card-body" style="height: 400px">
                                                     <div class="chartjs-size-monitor" style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;">
                                                         <div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;">
