b6341afa102e03097a5476735662b285ca234eb1
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-11-27 14:05:12 +0545

    data update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,10 @@ use Illuminate\Support\Facades\DB;
     <link href="{{ asset('css/piechart.css') }}" rel="stylesheet">
     <script src="{{ asset('js/piechart.js') }}" defer></script>
 
+    <!-- job icons for salary , level etc -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" integrity="sha256-3sPp8BkKUE7QyPSl6VfBByBroQbKxKG7tsusY2mhbVY=" crossorigin="anonymous" />
+
+
 
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css" integrity="sha256-3sPp8BkKUE7QyPSl6VfBByBroQbKxKG7tsusY2mhbVY=" crossorigin="anonymous" />
 
 
 <?php
