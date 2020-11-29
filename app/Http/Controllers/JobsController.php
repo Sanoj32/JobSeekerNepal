9c4ef8ff5a601888c656cf9aafd418a0ed090ecb
sanoj23 <sanoj.shrestha.13@gmail.com>
2020-11-29 16:15:41 +0545

    Data update code optimized

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -17,108 +17,96 @@ class JobsController extends Controller
             if (empty($jsondata)) {
                 continue;
             }
+
             foreach ($jsondata as $data) {
                 $storedjobs = Jobs::all();
-                $c          = 0; // if c=0 It means the url is a unique url from that site and filters duplicate jobs from same site
+
+                $c = 0; // if c=0 It means the url is a unique url from that site and filters duplicate jobs from same site
                 foreach ($storedjobs as $storedjob) {
                     if ($data['Page_URL'] == $storedjob['url']) {
                         $c = 1;
                     }
                 } //code to insert data in the database
                 if ($c == 0) {
-                    $jobs                = new Jobs();
-                    $jobs['name']        = $data['name'] ?? "";
-                    $jobs['company']     = $data['company'] ?? "";
-                    $jobs['logo']        = $data['logo'] ?? ""; // company logo
-                    $jobs['time']        = $data['time'] ?? ""; //full time or part time
-                    $jobs['level']       = $data['level'] ?? ""; // Senior or junoir
-                    $jobs['vacancy']     = $data['vacancy'] ?? "";
-                    $jobs['address']     = $data['address'] ?? "";
-                    $jobs['salary']      = $data['salary'] ?? "";
-                    $jobs['deadline']    = $data['deadline'] ?? "";
-                    $jobs['education']   = $data['education'] ?? "";
-                    $jobs['experience']  = $data['experience'] ?? "";
-                    $jobs['skills']      = $data['skills'] ?? "";
-                    $jobs['skills1']     = $data['skills1'] ?? "";
-                    $jobs['desct']       = $data['desct'] ?? "";
-                    $jobs['url']         = $data['Page_URL'] ?? "";
-                    $jobs['websitename'] = "";
-                    $jobs['isExpired']   = false;
-                    $jobs['isViewed']    = false;
-                    $jobs['isSaved']     = false;
-                    $jobs['relevancy']   = 10;
+                    $jobs                 = new Jobs();
+                    $jobs['name']         = $data['name'] ?? "";
+                    $jobs['company']      = $data['company'] ?? "";
+                    $jobs['logo']         = $data['logo'] ?? ""; // company logo
+                    $jobs['time']         = $data['time'] ?? ""; //full time or part time
+                    $jobs['level']        = $data['level'] ?? ""; // Senior or junoir
+                    $jobs['vacancy']      = $data['vacancy'] ?? "";
+                    $jobs['address']      = $data['address'] ?? "";
+                    $jobs['salary']       = $data['salary'] ?? "";
+                    $jobs['deadline']     = $data['deadline'] ?? "";
+                    $jobs['truedeadline'] = null;
+                    $jobs['education']    = $data['education'] ?? "";
+                    $jobs['experience']   = $data['experience'] ?? "";
+                    $jobs['skills']       = $data['skills'] ?? "";
+                    $jobs['skills1']      = $data['skills1'] ?? "";
+                    $jobs['desct']        = $data['desct'] ?? "";
+                    $jobs['url']          = $data['Page_URL'] ?? "";
+                    $jobs['websitename']  = $data['websitename'];
+                    $jobs['isExpired']    = false;
+                    $jobs['isViewed']     = false;
+                    $jobs['isSaved']      = false;
+                    $jobs['relevancy']    = 10;
                     $jobs->save();
                 }
             } // end code to insert data
         } //end of whole data collection from different websites
-        //code to assign websitename
-        $websitenames = array('np.linkedin.com', 'jobsnepal.com', 'globaljob.com', 'kumarijob.com', 'merojob.com', 'merorojgari.com', 'kathmandujobs.com', 'kantipurjob.com');
-        foreach ($websitenames as $sitename) {
-            $jobs = Jobs::where('url', 'ILIKE', '%' . $sitename . '%')->get();
-            foreach ($jobs as $job) {
-                $job->websitename = $sitename;
-                $job->save();
-            }
-        }
-        //end code to assign websitename
-
-        $jobs = Jobs::where('url', 'NOT ILIKE', '%merojob%')
-            ->where('url', 'NOT ILIKE', '%linkedin%')
-            ->get(); //catagroize legitimate dates and calculate exact date
         $datenow = Carbon::now('Asia/Kathmandu'); //the exact date of today
         $datenow->format('Y-m-d');
-        foreach ($jobs as $job) {
-            $deadline   = $job->deadline;
-            $deadline   = strtotime($deadline);
-            $deadline   = date('Y-m-d', $deadline); //formats the date into Y-m-d format
-            $comparison = date('1970-01-01');
-            if (!($deadline == $comparison)) {
-                $job->truedeadline = $deadline; //deadline is true deadline of the job
-                if ($deadline < $datenow) {
+        $jobs = Jobs::where('url', 'NOT ILIKE', '%linkedin%')
+            ->where('truedeadline', '=', null)
+            ->get(); //catagroize legitimate dates and calculate exact date
+        if ($jobs->count() > 0) {
+            foreach ($jobs as $job) {
+                $deadline   = $job->deadline;
+                $deadline   = strtotime($deadline);
+                $deadline   = date('Y-m-d', $deadline); //formats the date into Y-m-d format
+                $comparison = date('1970-01-01');
+                if (!($deadline == $comparison)) {
+                    $job->truedeadline = $deadline; //deadline is true deadline of the job
+                    if ($deadline < $datenow) {
+                        $job->isExpired = true;
+                    }
+                    $job->save();
+                }
+            }
+        }
+
+        $jobs = Jobs::where('url', 'ILIKE', '%linkedin%')
+            ->where('truedeadline', '=', null)
+            ->get();
+        if ($jobs->count() > 0) {
+            foreach ($jobs as $job) {
+                $job->isExpired    = false;
+                $deadline          = $job->deadline;
+                $deadline          = date('Y-m-d', strtotime($deadline));
+                $truedeadline      = date('Y-m-d', strtotime("+2 month", strtotime($deadline)));
+                $job->truedeadline = $truedeadline;
+                if ($job->truedeadline < $datenow) {
                     $job->isExpired = true;
                 }
                 $job->save();
             }
         }
-        //code to calculate the real date from string from merojob site
-        $jobs = Jobs::where('url', 'ILIKE', '%merojob%')->get();
-        foreach ($jobs as $job) {
-            $deadline          = $job->deadline;
-            $arr               = explode('(', $deadline);
-            $date              = $arr[0];
-            $actualdate        = strtotime($date);
-            $realdate          = date('Y-m-d', $actualdate);
-            $job->truedeadline = $realdate;
-            if ($realdate < $datenow) {
-                $job->isExpired = true;
-            }
-            $job->save();
-        }
-
-        $jobs = Jobs::where('url', 'ILIKE', '%linkedin%')->get();
-        foreach ($jobs as $job) {
-            $job->isExpired    = false;
-            $deadline          = $job->deadline;
-            $deadline          = date('Y-m-d', strtotime($deadline));
-            $truedeadline      = date('Y-m-d', strtotime("+2 month", strtotime($deadline)));
-            $job->truedeadline = $truedeadline;
-            if ($job->truedeadline < $datenow) {
-                $job->isExpired = true;
-            }
-            $job->save();
-        }
 
         //Adding one month to the posted date of merorojgari
-        $jobs = Jobs::where('url', 'ILIKE', '%merorojgari%')->get();
-        foreach ($jobs as $job) {
-            $job->isExpired    = false;
-            $deadline          = $job->deadline;
-            $truedeadline      = date('Y-m-d', strtotime("+1 month", strtotime($deadline)));
-            $job->truedeadline = $truedeadline;
-            if ($job->truedeadline < $datenow) {
-                $job->isExpired = true;
+        $jobs = Jobs::where('url', 'ILIKE', '%merorojgari%')
+            ->where('truedeadline', '=', null)
+            ->get();
+        if ($jobs->count() > 0) {
+            foreach ($jobs as $job) {
+                $job->isExpired    = false;
+                $deadline          = $job->deadline;
+                $truedeadline      = date('Y-m-d', strtotime("+2 month", strtotime($deadline)));
+                $job->truedeadline = $truedeadline;
+                if ($job->truedeadline < $datenow) {
+                    $job->isExpired = true;
+                }
+                $job->save();
             }
-            $job->save();
         }
 
         $jsondata = file_get_contents(public_path("jsondata") . "/queries.json");
